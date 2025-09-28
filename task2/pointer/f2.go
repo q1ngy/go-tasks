e043db17c3d14d9d2767dc046f47f5dba08bc7cb
waymore <930339770@qq.com>
2025-09-28 09:48:10 +0800

    task2

--- task2/pointer/f2.go
+++ task2/pointer/f2.go
@@ -1,12 +1,49 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-var wg sync.WaitGroup
-
-func main() {
-
+type Task func()
+type TaskResult struct {
+	d time.Duration
 }
 
-type ITask interface {
+func runTasks(tasks []Task) []TaskResult {
+	var wg sync.WaitGroup
+	result := make([]TaskResult, len(tasks))
+	for i, t := range tasks {
+		wg.Add(1)
+		go func(index int, task Task) {
+			defer wg.Done()
+			start := time.Now()
+			task()
+			end := time.Now()
+			d := end.Sub(start)
+			ts := TaskResult{d: d}
+			result[index] = ts
+		}(i, t)
+	}
+	wg.Wait()
+	return result
+}
+
+func main() {
+	tasks := []Task{
+		func() {
+			time.Sleep(1 * time.Second)
+		},
+		func() {
+			time.Sleep(2 * time.Second)
+		},
+		func() {
+			time.Sleep(3 * time.Second)
+		},
+	}
+	results := runTasks(tasks)
+	for _, t := range results {
+		fmt.Println(t.d)
+	}
 }
